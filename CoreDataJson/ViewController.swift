cfc916aee0f4bde6c9fcd0aa47094f28fa71843c
Joakim Sjöstedt <joakim.sjostedt@softhouse.se>
2020-03-12 12:41:27 +0100

    Initial Commit

---
+++ CoreDataJson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataJson
+//
+//  Created by Joakim Sjöstedt on 2020-03-12.
+//  Copyright © 2020 Joakim Sjöstedt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
